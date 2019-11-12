6419175c7a4b5d3bff9d539897110c1a54a1efda
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2019-11-12 23:46:21 +0530

    angular data binding and events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-events';
+  textValue = this.title;
+  isActive=false;
+  emaill="me@gmail.com";
+  onKeyUpEnterProperty()
+  {
+    console.log(this.emaill);
+  }
+   onClick($e)
+  {
+    console.log("button clicked", $e);
+  }
+  onClickEventBubbling($e)
+  {
+    $e.stopPropagation();// add this to stop event bubbling to div
+    console.log("button clicked");
+  }
+  onDivClick()
+  {
+    console.log("div clicked");
+  }
+  onKeyUp($e)
+  {
+    if($e.keyCode===13)
+    {
+      console.log("enter key pressed");
+    }
+  }
+  onKeyUpEnter($event)
+  {
+    console.log("enter was pressed");
+    console.log($event.target.value);
+  }
+
+  onKeyUpEnterTemplate(email)
+  {
+    console.log(email);
+  }
 }
